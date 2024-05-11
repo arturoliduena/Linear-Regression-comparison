41f7fc6a2f0caa4f1555e8a93c48ed925efda3e8
csanchezest <cristian.sanchez.estape@estudiantat.upc.edu>
2024-05-11 13:02:05 +0200

    Formatted code

--- src/ridge_estimator_calculation.py
+++ src/ridge_estimator_calculation.py
@@ -2,26 +2,30 @@ from sklearn.datasets import fetch_california_housing
 from sklearn.preprocessing import StandardScaler
 from scipy.linalg import eig, inv
 import statsmodels.api as sm
-import pandas as pd
 import numpy as np
 
+
 def compute_k1(p, var, alpha_OLS):
-    k_1=p*var/(alpha_OLS.T*alpha_OLS)
+    k_1 = p*var/(alpha_OLS.T*alpha_OLS)
     x = k_1.tolist()[0][0]
     print("k_1 estimator:\t\t{}".format(x))
     return x
 
+
 def compute_k1_AD(var, alpha_OLS, lambda_max):
-    k_1AD = 2/(8*lambda_max) * np.sum(var/alpha_OLS.A1**2 )
+    k_1AD = 2/(8*lambda_max) * np.sum(var/alpha_OLS.A1**2)
     print("k_1(AD) estimator:\t{}".format(np.real(k_1AD)))
+
 
 def compute_k2_AD(var, alpha_OLS, lambda_max):
     k_2AD = np.median([2*var/(lambda_max*alpha_OLS.A1**2)])
     print("k_2(AD) estimator:\t{}".format(np.real(k_2AD)))
 
+
 def compute_k3_AD(p, var, alpha_OLS, lambda_max):
     k_3AD = 2*var/(lambda_max*np.prod([x**2 for x in alpha_OLS])**(1/p))
     print("k_3(AD) estimator:\t{}".format(np.real(k_3AD.tolist()[0][0])))
+
 
 def compute_k4_AD(p, var, alpha_OLS, lambda_max):
     k_4AD = (2*p/lambda_max) * sum([var/x**2 for x in alpha_OLS])
@@ -49,7 +53,7 @@ if __name__ == "__main__":
     # Extract the target value and prepare for future operations
     Y = np.asmatrix(df_standarized.MedHouseVal.values).T
     # Compute the OLS of the data; it will be used as reference
-    model = sm.OLS(df_standarized.MedHouseVal,df_standarized[data_columns])
+    model = sm.OLS(df_standarized.MedHouseVal, df_standarized[data_columns])
     results = model.fit()
     # Get the betas corresponding to the OLS fit and set dimensions correctly
     beta_OLS = np.asmatrix(results.params).T
@@ -62,10 +66,9 @@ if __name__ == "__main__":
     compute_k1(p, var, alpha_OLS)
 
     compute_k1_AD(var, alpha_OLS, lambda_max)
-    
+
     compute_k2_AD(var, alpha_OLS, lambda_max)
 
     compute_k3_AD(p, var, alpha_OLS, lambda_max)
-    
-    compute_k4_AD(p, var, alpha_OLS, lambda_max)
 
+    compute_k4_AD(p, var, alpha_OLS, lambda_max)
