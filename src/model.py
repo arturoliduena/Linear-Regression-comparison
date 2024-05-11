4303cbcd3cd7aced9d0dc565e97978801ea1c394
csanchezest <cristian.sanchez.estape@estudiantat.upc.edu>
2024-05-11 13:01:29 +0200

    Model modifications to output alpha performances

--- src/model.py
+++ src/model.py
@@ -1,36 +1,42 @@
-from sklearn.linear_model import Ridge, Lasso
-from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression, Lasso, Ridge
 from sklearn.model_selection import KFold, cross_val_score
 from sklearn.datasets import fetch_california_housing
+from sklearn.linear_model import Ridge, Lasso
+
 
 def models():
-  california_housing = fetch_california_housing()
-  print(california_housing.DESCR)
-  # Define a dictionary of estimators with their hyperparameters
-  estimators = {
-    'LinearRegression': (LinearRegression(), {}),
-    'Lasso': (Lasso(alpha=0.1), {'alpha': 0.1}),
-    'Ridge': (Ridge(alpha=1.0), {'alpha': 1.0})
-  }
+    california_housing = fetch_california_housing()
+    alphas = [("LassoCV", 0.0015953096839947316), ("LassoLarsCV", 0.0012314644830673947), ("k1", 1.7229933519274163),
+              ("k1(AD)", 0.0010845229813710349), ("k2(AD)", 0.00016990296535820582), ("k3(AD)", 0.0001779808406195281), ("k4(AD)", 0.06940947080774623)]
+    # Define a dictionary of estimators with their hyperparameters
+    estimators = {
+        'LinearRegression': (LinearRegression, [(None, None)]),
+        'Lasso': (Lasso, alphas),
+        'Ridge': (Ridge, alphas)
+    }
 
-  # Define evaluation metrics
-  scoring_metrics = ['r2', 'neg_mean_squared_error', 'explained_variance']
+    # Define evaluation metrics
+    scoring_metrics = ['r2', 'neg_mean_squared_error', 'explained_variance']
 
-  # Iterate over each estimator
-  for estimator_name, (estimator_object, hyperparameters) in estimators.items():
-    print(f"\n{estimator_name} Hyperparameters: {hyperparameters}")
-    
-    # Iterate over each scoring metric
-    for metric in scoring_metrics:
-      kfold = KFold(n_splits=10, random_state=11, shuffle=True)
-      
-      # Perform cross-validation
-      scores = cross_val_score(estimator=estimator_object, X=california_housing.data, y=california_housing.target, cv=kfold, scoring=metric)
-      
-      # Print the evaluation metric
-      print(f'{metric:>25}: mean score={scores.mean():.3f}, std={scores.std():.3f}')
+    # Iterate over each estimator
+    for estimator_name, (estimator_object, hyperparameters) in estimators.items():
+
+        for (name, alpha) in hyperparameters:
+            x = ("" if alpha is None else name)
+            print(f"\n{estimator_name} Hyperparameters: {x}")
+
+            # Iterate over each scoring metric
+            for metric in scoring_metrics:
+                kfold = KFold(n_splits=10, random_state=11, shuffle=True)
+
+                # Perform cross-validation
+                scores = cross_val_score(estimator=(estimator_object(alpha=alpha) if alpha != None else estimator_object()), X=california_housing.data,
+                                         y=california_housing.target, cv=kfold, scoring=metric)
+
+                # Print the evaluation metric
+                print(
+                    f'{metric:>25}: mean score={scores.mean():.3f}, std={scores.std():.3f}')
 
 
 if __name__ == "__main__":
-  models()
+    models()
